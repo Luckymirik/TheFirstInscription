3278bb637e28f9a76da373a2e045e4380c9d4104
Miroslav Tsadkin <Luckymirik@mail.ru>
2024-03-21 16:16:22 +0300

    Finish

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
+        JFrame frame = new JFrame("Окно");
+        JLabel label = new JLabel("My first inscription");
+        Font font = new Font("Vladimir script",Font.BOLD,50);
+        label.setFont(font);
+        frame.add(label);
+        frame.setSize(500,300);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
 
+      }
     }
-}
